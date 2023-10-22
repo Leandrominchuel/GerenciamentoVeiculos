47823c9a6d50d440d71761057514ee5d0188f427
Leandro Minchuel <minchuel@hotmail.com>
2023-10-22 16:37:15 -0300

    Criando Classe 'Cliente'

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,18 @@
+public class Cliente {
+    private String nome;
+    private String sobrenome;
+    private String rg;
+    private String cpf;
+    private String endereco;
+
+    // Construtor
+    public Cliente(String nome, String sobrenome, String rg, String cpf, String endereco) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.rg = rg;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    // Getters e Setters (métodos para acessar e modificar os atributos)
+}
